99613c4d915d4257016e8ceff573069442613358
Prashanth <kumardvplr@gmail.com>
2020-06-04 23:30:29 -0400

    added goals view

--- App.js
+++ App.js
@@ -1,19 +1,69 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TextInput, Button, ScrollView } from 'react-native';
 
 export default function App() {
+  const [enteredGoal, setEnteredGoal] = useState('');
+  const [courseGoals, setCourseGoals] = useState([]);
+
+  const goalInputHandler = (enteredText) => {
+    setEnteredGoal(enteredText);
+  };
+
+  const addGoalHandler = () => {
+    setCourseGoals((currentGoals) => [...currentGoals, enteredGoal]);
+    setEnteredGoal('');
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={styles.screen}>
+      <View style={styles.inputContainer}>
+        <TextInput
+          placeholder="Course Goal"
+          style={styles.input}
+          onChangeText={goalInputHandler}
+          value={enteredGoal}
+        />
+        <Button style={styles.btn} onPress={addGoalHandler} title="ADD" />
+        
+      </View>
+      <ScrollView>
+        {courseGoals.map((goal, index) => (
+          <View key={index} style={styles.listItem}>
+            <Text>{goal}</Text>
+          </View>
+        ))}
+      </ScrollView>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  screen: {
+    padding: 50,
   },
+  inputContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  input: {
+    borderColor: 'black',
+    borderWidth: 1,
+    padding: 10,
+    width: '70%',
+  },
+  btn: {
+    padding: 50,
+    borderWidth: 2,
+    borderColor: 'black',
+    borderRadius: 2,
+    color: 'green',
+  },
+  listItem: {
+    padding: 10,
+    marginVertical:10,
+    borderColor: 'black',
+    backgroundColor: '#ccc',
+    borderWidth: 1
+  }
 });
